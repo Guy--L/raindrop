fa5561c2366ec65707e13acad02205a478391525
Guy..Lister <github@listerguy.com>
2017-10-16 22:57:45 -0400

    Add publishing
    Remove application insights.
    Add publishing profile.

--- raindrop/Program.cs
+++ raindrop/Program.cs
@@ -17,7 +17,6 @@ namespace raindrop
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
-                .UseApplicationInsights()
                 .Build();
 
             host.Run();
